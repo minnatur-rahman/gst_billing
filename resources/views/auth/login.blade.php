ac9b7cafa6c7779fced69d0e0cb6f2ff9e29599f
minnatur-rahman <minnatur7373@gmail.com>
2024-11-08 07:32:48 +0600

    adfh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
-        <a href="forgot-password">I forgot my password</a>
+        <a href="{{ url('forgot-password') }}">I forgot my password</a>
       </p>
       <p class="mb-0">
         <a href="{{ url('register') }}" class="text-center">Register a new membership</a>
