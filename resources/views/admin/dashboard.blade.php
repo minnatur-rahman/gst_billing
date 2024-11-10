559b4278f7a106dc9a5bc053d311ac3996407cb2
minnatur-rahman <minnatur7373@gmail.com>
2024-11-10 23:18:53 +0600

    asfdh

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,9 +21,9 @@
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{ url('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ url('public') }}/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="{{ url('plugins/daterangepicker/daterangepicker.css') }}">
   <!-- summernote -->
-  <link rel="stylesheet" href="{{ url('public') }}/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="{{ url('plugins/summernote/summernote-bs4.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -1341,23 +1341,23 @@
 <script src="plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
 <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="{{ url('public') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ url('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{ url('public') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ url('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="{{ url('public') }}/plugins/moment/moment.min.js"></script>
-<script src="{{ url('public') }}/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ url('plugins/moment/moment.min.js') }}"></script>
+<script src="{{ url('plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ url('public') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ url('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="{{ url('public') }}/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ url('plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{ url('public') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ url('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ url('public') }}/dist/js/adminlte.js"></script>
+<script src="{{ url('dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ url('public') }}/dist/js/pages/dashboard.js"></script>
+<script src="{{ url('dist/js/pages/dashboard.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ url('public') }}/dist/js/demo.js"></script>
+<script src="{{ url('dist/js/demo.js') }}"></script>
 </body>
 </html>
