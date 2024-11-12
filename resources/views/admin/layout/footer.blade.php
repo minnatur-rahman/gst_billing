28848918ef1047a3bf3dc7f3793db0b13829e7e5
minnatur-rahman <minnatur7373@gmail.com>
2024-11-12 06:41:51 +0600

    asdh

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024-2019 <a target="_blank" href="https://github.com/minnatur-rahman">Minnatur Rahman GitHub</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.1-pre
+    </div>
+  </footer>
