b906c06ed5a19c85edfb187cfacfe36d9043e73d
minnatur-rahman <minnatur7373@gmail.com>
2024-11-12 07:15:06 +0600

    lashfd

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,5 +1,4 @@
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="{{ url('dist/img/AdminLTELogo.png') }}" alt="GST Logo" class="brand-image img-circle elevation-3"
@@ -15,21 +14,24 @@
           <img src="{{ url('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Admin</a>
         </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-          <li class="nav-item">
-            <a href="{{ url('admin/dashboard') }}" class="nav-link active">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
+                <i class="right fas fa-angle-left"></i>
               </p>
             </a>
+
           </li>
          
           <li class="nav-item">
@@ -40,7 +42,7 @@
               </p>
             </a>
           </li>
-          
+        
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
