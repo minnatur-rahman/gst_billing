e2aa9f42d4b12106f5f6bbd4b67007080fbe9ce4
minnatur-rahman <minnatur7373@gmail.com>
2024-11-12 21:49:07 +0600

    lashj

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -35,31 +35,7 @@
   <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper">
   
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-        </li>
-       
-      </ul>
   
-      <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
-       
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
-            <i class="fas fa-sign-out-alt"></i>
-            
-          </a>
-          
-        </li>
-       
-      </ul>
-    </nav>
-    <!-- /.navbar -->
   
 @include('admin.layout.sidebar')
 
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,3 +1,30 @@
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+     
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+     
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link"  href="{{ url('logout') }}">
+          <i class="fas fa-sign-out-alt"></i>
+          
+        </a>
+        
+      </li>
+     
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
