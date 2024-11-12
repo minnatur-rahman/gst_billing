5d474251e65ad6b922b0cecc96d98df607425023
minnatur-rahman <minnatur7373@gmail.com>
2024-11-12 06:36:43 +0600

    lah

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -194,7 +194,7 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2024-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2024-2019 <a target="_blank" href="https://github.com/minnatur-rahman">Minnatur Rahman GitHub</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.1-pre
