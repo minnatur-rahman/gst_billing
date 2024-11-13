fec3fb67b4ddd423bfad8185ffab9e30af2f346f
minnatur-rahman <minnatur7373@gmail.com>
2024-11-13 22:51:25 +0600

    sfj

--- resources/views/admin/parties_type/list.blade.php
+++ resources/views/admin/parties_type/list.blade.php
@@ -9,14 +9,9 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Simple Tables</h1>
+            <h1>Parties Table</h1>
           </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Simple Tables</li>
-            </ol>
-          </div>
+         
         </div>
       </div><!-- /.container-fluid -->
     </section>
@@ -28,7 +23,7 @@
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Bordered Table</h3>
+                <h3 class="card-title">Parties type lists</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -108,8 +103,5 @@
   <!-- /.content-wrapper -->
 </div>
 <!-- ./wrapper -->
-
-
-
 
 @endsection
