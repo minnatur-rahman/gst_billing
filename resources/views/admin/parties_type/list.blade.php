c87986e98ee05ff37cd0422de0063327fc8a93a1
minnatur-rahman <minnatur7373@gmail.com>
2024-11-14 23:33:43 +0600

    lasdfh

--- resources/views/admin/parties_type/list.blade.php
+++ resources/views/admin/parties_type/list.blade.php
@@ -42,7 +42,7 @@
                       <td>Update Software</td>
                       <td>
                         <a href="" class="btn btn-info"><i class="fas fa-pencil-alt"></i></a>
-                        <a href="" class="btn btn-info><i class="fas fa-trash"></i></a>
+                        <a href="" class="btn btn-danger><i class="fas fa-trash"></i></a>
                       </td>
                     </tr>
                   </tbody>
