dca1865dc11cead48e13636632b76ee0a5974603
minnatur-rahman <minnatur7373@gmail.com>
2024-11-14 22:57:41 +0600

    lashf

--- resources/views/admin/parties_type/add.blade.php
+++ resources/views/admin/parties_type/add.blade.php
@@ -9,25 +9,25 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>General Form</h1>
+            <h1>Add Parties Type</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">General Form</li>
+              <li class="breadcrumb-item active">Add Parties Type</li>
             </ol>
           </div>
         </div>
         <div class="card card-info">
             <div class="card-header">
-              <h3 class="card-title">Horizontal Form</h3>
+              <h3 class="card-title">Add Parties Type</h3>
             </div>
             <!-- /.card-header -->
             <!-- form start -->
             <form class="form-horizontal">
               <div class="card-body">
                 <div class="form-group row">
-                  <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Parties Type Name</label>
                   <div class="col-sm-10">
                     <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                   </div>
