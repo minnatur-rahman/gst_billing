8fb5db867ca5c22af3bc6b8af6da89dc32caef88
minnatur-rahman <minnatur7373@gmail.com>
2024-11-16 06:23:48 +0600

    lasf

--- resources/views/_message.blade.php
+++ resources/views/_message.blade.php
@@ -1,8 +1,16 @@
-@if (!empty(session('success')))
+{{-- @if (!empty(session('success')))
     <div class="alert alert-success" role="alert">
         {{ session('success') }}
     </div>
+@endif --}}
+
+@if(Session::has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ Session::get('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 @endif
+
 
 @if (!empty(session('error')))
     <div class="alert alert-danger" role="alert">
